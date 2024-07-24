3c4be7e2b224978255fb3e3c3036224c61799f00
bogdanncalin <bogdanncalin@yahoo.com>
2024-07-24 20:31:28 +0300

    Install json-server on project

--- src/components/movie-details/MovieDetails.jsx
+++ src/components/movie-details/MovieDetails.jsx
@@ -1,14 +1,36 @@
 import { movies } from "../../data/data";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
+import "./MovieDetails.css";
+import { useEffect } from "react";
 
 export default function MovieDetails() {
   let { id } = useParams();
 
   const selectedMovie = movies.find((movie) => movie.id === id);
-  console.log({selectedMovie});
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!selectedMovie) {
+      navigate("/");
+    }
+  }, []);
+  
+  if (!selectedMovie) {
+    //   return <h1>404: Selected movie does not exist!</h1>; or
+    return;
+  }
+
+  const { title, imageUrl, category } = selectedMovie;
+
   return (
-    <>
-      
-    </>
+    <section>
+      <header>
+        <h3> {title} </h3>
+      </header>
+
+      <img src={imageUrl} />
+
+      <p className="movie-detail__category">Category: {category}</p>
+    </section>
   );
 }
